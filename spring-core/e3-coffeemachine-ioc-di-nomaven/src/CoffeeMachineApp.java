c0171b5add325a41e393599290ac53e593a79ca7
craft-code-dec <crafted.coded.2003@gmail.com>
2020-03-02 15:48:35 +0530

    .

---
+++ spring-core/e3-coffeemachine-ioc-di-nomaven/src/CoffeeMachineApp.java
@@ -0,0 +1,47 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CoffeeMachineApp {
+	public static void main(String[] args) {
+		HotWater hotWater = null;
+		Boost boost = null;
+		
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		hotWater = applicationContext.getBean("hotWater",HotWater.class);
+		hotWater.getHotWater();
+		
+		boost = applicationContext.getBean("boost",Boost.class);
+		boost.getBoost();
+		
+		System.out.println(boost);
+		Boost boost1 = applicationContext.getBean("boost",Boost.class);
+		System.out.println(boost1);
+	}
+}
+
+class HotWater {
+	public void getHotWater() {
+		System.out.println("Hot Hot Water for the Smarter!");
+	}
+}
+class Boost {
+private Sugar sugar;
+private Milk milk;
+	public Boost(Sugar sugar, Milk milk) {
+		this.sugar = sugar;
+		this.milk = milk;
+	}
+	public void getBoost() {
+		System.out.println("Boost made of " + sugar.sugarType +" and " + milk.milkType);
+	}
+}
+class Sugar{
+	public String sugarType = "Brown Sugar!";
+}
+class Milk {
+	public String milkType = "Fresh Milk!";
+}
+//challenge : try adding this as dependency for boost
+class CharcoalMilk {
+	public String milkType = "Charcoal Milk!";
+}
